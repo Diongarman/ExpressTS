2251b8d4277c3637f0a9302d45af1995efd93a60
Dion Garman <diongarman@Dions-MacBook-Pro.local>
2020-07-18 19:12:42 +0100

    temp added my own post login route handler

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from "express";
 import { router } from "./routes/loginRoutes";
+var bodyParser = require("body-parser");
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(router);
 
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,30 @@
 import { Router, Request, Response } from "express";
 
-const router = Router();
+const users = { user1: "password" };
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("hello madam");
+const router = Router();
+router.get("/", (req: Request, res: Response) => res.send("hi there"));
+router.get("/login", (req: Request, res: Response) => {
+  res.send(
+    `<form method="POST">
+        <div>
+            <label>Email</label>
+            <input name='email'></input>
+        </div>
+        <div>
+            <label>Password</label>
+            <input name='password' type='password'></input>
+        </div>
+
+        <button>Submit</button>
+      </form>`
+  );
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  const { email } = req.body;
+  console.log(req.body);
+  res.send(`Hell ${email}`);
 });
 
 export { router };
