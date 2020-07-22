b7da4bbdae4287d63545ecc5568f555a6939e4f8
Dion Garman <diongarman@Dions-MacBook-Pro.local>
2020-07-22 13:32:15 +0100

    LoginController class

---
+++ src/controllers/loginController.ts
@@ -0,0 +1,22 @@
+import { Router, Request, Response, NextFunction } from "express";
+
+@Controller("")
+class LoginController {
+  @get("/login")
+  getLogin(path: string, req: Request, res: Response): void {
+    res.send(
+      `<form method="POST">
+                <div>
+                    <label>Email</label>
+                    <input name='email'></input>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input name='password' type='password'></input>
+                </div>
+        
+                <button>Submit</button>
+                </form>`
+    );
+  }
+}
